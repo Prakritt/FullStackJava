dffb596c3e8fbfcec4da1ac8f9484d0ba4b0e5f4
Prakrit Uprety <prakritupreety0@gmail.com>
2024-10-07 17:02:32 -0400

    Create role entity and Update many-to-may relationship between entity and role with user as owning entity

---
+++ book-network/src/main/java/com/krit/book/role/Role.java
@@ -0,0 +1,45 @@
+package com.krit.book.role;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.krit.book.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class Role {
+
+    @Id
+    @GeneratedValue
+    private Integer Id;
+
+    @Column(unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    @JsonIgnore
+    private List<User> users;
+
+    @CreatedDate
+    @Column(nullable = false,updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime lastModifiedDate;
+
+
+
+
+}
